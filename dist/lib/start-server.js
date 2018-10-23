5816564fb2494f6197996864f208f463e3c6912c
Andrii U <un0rsk@users.noreply.github.com>
2018-10-23 08:31:40 +0200

    Feature/context path (#7)
    * add context-path
    * debug requests
    * set content type for settings.js

--- dist/lib/start-server.js
+++ dist/lib/start-server.js
@@ -19,26 +19,36 @@ var startServer = function startServer(server, html) {
     }
   };
 
-  server.get('/index.html', function (req, res) {
+  var CONTEXT_PATH = process.env.CONTEXT_PATH || '';
+
+  if (process.env.FRONT_SHELL_DEBUG) {
+    server.use(function (req, res, next) {
+      console.log(req.url);
+      next();
+    });
+  }
+
+  server.get(CONTEXT_PATH + '/index.html', function (req, res) {
     res.send(html);
   });
 
-  server.get('/', function (req, res) {
+  server.get(CONTEXT_PATH + '/', function (req, res) {
     res.send(html);
   });
 
-  server.get('/settings.js', function (req, res) {
+  server.get(CONTEXT_PATH + '/settings.js', function (req, res) {
     var settingsPath = process.env.FRONTSHELL_SETTINGS_PATH ? fs.readFileSync(process.env.FRONTSHELL_SETTINGS_PATH, 'utf8') : null;
     var settingsName = process.env.FRONTSHELL_SETTINGS_NAME ? process.env.FRONTSHELL_SETTINGS_NAME : null;
+    res.type('.js');
     res.send(renderEnvSettingsFile(settingsPath, settingsName));
   });
 
-  server.use(express.static(process.env.WEB_ROOT || 'build', { index: false }));
+  server.use(CONTEXT_PATH, express.static(process.env.WEB_ROOT || 'build', { index: false }));
 
-  server.get('/health/isAlive', function (req, res) {
+  server.get(CONTEXT_PATH + '/health/isAlive', function (req, res) {
     return res.sendStatus(200);
   });
-  server.get('/health/isReady', function (req, res) {
+  server.get(CONTEXT_PATH + '/health/isReady', function (req, res) {
     return res.sendStatus(200);
   });
 
--- src/lib/start-server.js
+++ src/lib/start-server.js
@@ -17,24 +17,34 @@ const startServer = (server, html) => {
     }
   }
   
-  server.get('/index.html', (req, res) => {
+  const CONTEXT_PATH = process.env.CONTEXT_PATH || '';
+
+  if (process.env.FRONT_SHELL_DEBUG) {
+    server.use(function (req, res, next) {
+      console.log(req.url);
+      next();
+    });
+  }
+
+  server.get(`${CONTEXT_PATH}/index.html`, (req, res) => {
     res.send(html);
   });
 
-  server.get('/', (req, res) => {
+  server.get(`${CONTEXT_PATH}/`, (req, res) => {
     res.send(html);
   });
 
-  server.get('/settings.js', (req, res) => {
+  server.get(`${CONTEXT_PATH}/settings.js`, (req, res) => {
     const settingsPath = process.env.FRONTSHELL_SETTINGS_PATH ? fs.readFileSync(process.env.FRONTSHELL_SETTINGS_PATH, 'utf8') : null;
     const settingsName = process.env.FRONTSHELL_SETTINGS_NAME ? process.env.FRONTSHELL_SETTINGS_NAME : null;
+    res.type('.js');
     res.send(renderEnvSettingsFile(settingsPath, settingsName));
   });
 
-  server.use(express.static(process.env.WEB_ROOT || 'build', {index: false}));
+  server.use(CONTEXT_PATH, express.static(process.env.WEB_ROOT || 'build', {index: false}));
 
-  server.get('/health/isAlive', (req, res) => res.sendStatus(200));
-  server.get('/health/isReady', (req, res) => res.sendStatus(200));
+  server.get(`${CONTEXT_PATH}/health/isAlive`, (req, res) => res.sendStatus(200));
+  server.get(`${CONTEXT_PATH}/health/isReady`, (req, res) => res.sendStatus(200));
 
   const port = process.env.PORT || 8080;
   server.listen(port, () => {
